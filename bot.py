411d9e6934305ed7766d18f7d47652903cd3543c
ScottT2-spec <ScottT2-spec@users.noreply.github.com>
2026-02-23 04:51:26 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ logger = logging.getLogger(__name__)
 #
 # Constants / paths
 #
-TOKEN = "YOUR TOKEN"
+TOKEN = "8546969297:AAGre3cx16LVPeHijJEO86X-pN5Z_CX4_LQ"
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(SCRIPT_DIR, "users.db")
 CHUNK_SIZE = 3500  # max chars per Telegram message
